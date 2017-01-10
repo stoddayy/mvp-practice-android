8f3fc57fc12d316c58ac643f2da7a6c9d0a6f734
MichaelStoddart <michael@apposing.co.uk>
2017-01-10 11:39:58 +0000

    Added features packages and activities

---
+++ app/src/main/java/com/mstoddart/mvppracticeapp/loginsignup/LoginSignUpActivity.java
@@ -0,0 +1,15 @@
+package com.mstoddart.mvppracticeapp.loginsignup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mstoddart.mvppracticeapp.R;
+
+public class LoginSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sign_up);
+    }
+}
--- app/src/main/java/com/mstoddart/mvppracticeapp/pokemons/PokemonActivity.java
+++ app/src/main/java/com/mstoddart/mvppracticeapp/pokemons/PokemonActivity.java
@@ -1,9 +1,11 @@
-package com.mstoddart.mvppracticeapp;
+package com.mstoddart.mvppracticeapp.pokemons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.mstoddart.mvppracticeapp.R;
+
+public class PokemonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
